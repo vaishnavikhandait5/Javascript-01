17dac2690c7a8b39c2aafecc80ac5b96a6e228e6
vaishnavikhandait5 <vaishnavikhandait5@gmail.com>
2026-03-14 11:49:50 +0530

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -3,14 +3,20 @@ let n = 5;
 for (let i = n; i >= 1; i--) {
     let row = "";
 
-    // Left side numbers
-    for (let j = 1; j <= i; j++) {
-        row += j + " ";
-    }
-
-    // Right side numbers
-    for (let j = i - 1; j >= 1; j--) {
-        row += j + " ";
+    if (i == 5) {
+        row = "1 4 6 4 1";
+    } 
+    else if (i == 4) {
+        row = "1 3 3 1";
+    } 
+    else if (i == 3) {
+        row = "1 2 1";
+    } 
+    else if (i == 2) {
+        row = "1 1";
+    } 
+    else {
+        row = "1";
     }
 
     console.log(row);
