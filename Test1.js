67d5629fff39525f4ff02711aecc251ce442de42
vaishnavikhandait5 <vaishnavikhandait5@gmail.com>
2026-03-14 11:46:57 +0530

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,1 +1,17 @@
+let n = 5;
 
+for (let i = n; i >= 1; i--) {
+    let row = "";
+
+    // Left side numbers
+    for (let j = 1; j <= i; j++) {
+        row += j + " ";
+    }
+
+    // Right side numbers
+    for (let j = i - 1; j >= 1; j--) {
+        row += j + " ";
+    }
+
+    console.log(row);
+}
